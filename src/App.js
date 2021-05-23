bf23397f7f151bb1f67bca985fa811aec03fcd38
Ranjan <ranjanrajthala24@gmai.com>
2021-05-23 14:30:13 +0545

    Adding action and Action file in project

--- src/App.js
+++ src/App.js
@@ -10,28 +10,31 @@ import cartItems from "./cart-items";
 // reducer - function that used to update store
 
 import { createStore } from "redux";
+import { DECREASE, INCREASE } from "./action";
 
 //initial store
 const init = {
   count: 24,
+  name: "moblie",
+  brand: "multiper",
 };
 
 // reducer
 function reducer(state, action) {
   console.log(state, action);
-  if (action.type === "DECREASE") {
-    return { count: state.count - 1 };
+  if (action.type === DECREASE) {
+    return { ...state, count: state.count - 1 };
   }
+  if (action.type === INCREASE) {
+    return { ...state, count: state.count + 1, brand: "google" };
+  }
+
   return state;
 }
 
 const store = createStore(reducer, init);
 store.dispatch({ type: "DECREASE" });
-store.dispatch({ type: "DECREASE" });
-store.dispatch({ type: "DECREASE" });
-store.dispatch({ type: "DECREASE" });
-store.dispatch({ type: "DECREASE" });
-
+store.dispatch({ type: "INCREASE" });
 function App() {
   // cart setup
 
