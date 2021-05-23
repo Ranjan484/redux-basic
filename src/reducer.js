9d182b1ee38f382c85bdab16e90a67ea1b095add
Ranjan <ranjanrajthala24@gmai.com>
2021-05-23 15:50:23 +0545

    added fun(clear item)

--- src/reducer.js
+++ src/reducer.js
@@ -1,4 +1,9 @@
+import { CLEAR_CART } from "./action";
+
 function reducer(state, action) {
+  if (action.type === CLEAR_CART) {
+    return { ...state, cart: [] };
+  }
   return state;
 }
 export default reducer;
