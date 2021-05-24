d085ee80ce5fc2cc9fbc29a5241937b9f56fb60f
Ranjan <ranjanrajthala24@gmai.com>
2021-05-24 10:08:06 +0545

    remove func added

--- src/reducer.js
+++ src/reducer.js
@@ -1,8 +1,20 @@
-import { CLEAR_CART } from "./action";
+import { CLEAR_CART, DECREASE, INCREASE, REMOVE } from "./action";
 
 function reducer(state, action) {
   if (action.type === CLEAR_CART) {
     return { ...state, cart: [] };
+  }
+  if (action.type === INCREASE) {
+    console.log("increase");
+  }
+  if (action.type === DECREASE) {
+    console.log("decrease");
+  }
+  if (action.type === REMOVE) {
+    return {
+      ...state,
+      cart: state.cart.filter((cartItem) => cartItem.id !== action.payload.id),
+    };
   }
   return state;
 }
